c269b7ae2332b6acbf3ff4a9e9f0773443c597b1
Igor <igorgoulartoliveira96@gmail.com>
2020-01-12 12:49:25 -0300

    - comandos do go - download e instalação de pacotes

---
+++ fundamentos/comandos/comandos.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+/**
+go get --> download de pacotes
+go version --> versão do go
+go env --> informações do ambiente
+go doc cmd/vet --> análise do código
+*/
+
+func main() {
+	fmt.Printf("Outro programa em %s!", "GO")
+}
